1145213c84a315e80940b1b363c09ac0bd0af95c
אהוד סמידוברסקי <ehud_semi>
2025-11-18 23:29:54 +0200

    solved the 4 task, check if most of the numbers are odd or even

--- ex1.c
+++ ex1.c
@@ -22,9 +22,9 @@ int main()
     printf("Please enter a character\n");
     char inputCharacter;
     scanf(" %c", &inputCharacter);
-    int value = (int)inputCharacter;
+    int value = (int)inputCharacter;    //ascii value
     printf("Its numerical value is: %d\n", value);
-    int oddOrEven = value & 1;
+    int oddOrEven = value & 1;      //check the last bit if 1 or 0
     printf("0 for even, 1 for odd: %d\n", oddOrEven);
 
 
@@ -40,7 +40,7 @@ int main()
     printf("Please enter a negative integer\n");
     int inputNum;
     scanf(" %d", &inputNum);
-    int onesComplement = -(~inputNum);
+    int onesComplement = -(~inputNum);   //convert from 2's complement to 1's
     printf("1's complement: %d\n", onesComplement);
     unsigned unsignedNumber = (unsigned)inputNum;
     printf("unsigned: %u\n", unsignedNumber);
@@ -58,7 +58,7 @@ int main()
     printf("Please enter 3 integers\n");
     int inputNum2, rightShift, leftShift;
     scanf(" %d%d%d", &inputNum2, &rightShift, &leftShift );
-    int shiftedNum = (inputNum2 >> rightShift) << leftShift;
+    int shiftedNum = (inputNum2 >> rightShift) << leftShift;    //first i shift right and then left
     printf("After shifting right and left: %d\n", shiftedNum);
 
 
@@ -66,13 +66,32 @@ int main()
 
 
 
-        
-
     // Even - Odd
     printf("\nEven - Odd:\n");
     /* Scan 3 Integers.
     If at least two of them are even - print 0.
     If at least two of them are odd - print 1. */
+    printf("Please enter 3 integers\n");
+    int firstNum, secondNum, thirdNum;
+    scanf("%d %d %d", &firstNum, &secondNum, &thirdNum);
+    int oddOrEvenFirstNum = firstNum & 1;
+    int oddOrEvenSecondNum = secondNum & 1;
+    int oddOrEvenThirdNum = thirdNum & 1;
+    int sum = oddOrEvenFirstNum + oddOrEvenSecondNum + oddOrEvenThirdNum;
+    /* If sum >= 2 - at least two odd
+    If sum <= 1 - at least two even
+    (sum >> 1) & 1 gives 1 if sum>=2, 0 otherwise */
+    int mostAre = (sum >> 1) & 1;
+    printf("0 - most of them are even, 1 - most of them are odd: %d\n", mostAre);
+
+
+
+
+
+
+
+
+
 
     // Different Bases
     printf("\nDifferent Bases:\n");
